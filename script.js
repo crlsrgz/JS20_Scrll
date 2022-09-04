7a9ce8fc7d9881745adc9ee0041908984980beea
crls <crls@fake>
2022-09-04 22:08:24 +0200

    end: Finished project

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
+
+
 const imageContainer = document.getElementById('image-container');
-const imageLoader = document.getElementById('image-loader');
+const pageLoader = document.getElementById('page-loader');
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages= 0;
 
 let photosArray = [];
 
 // unplash API
-const photosToLoad = 5;
+const photosToLoad = 3;
+const imageSize = "regular";
 const apiKey = '7B31_IL-q-490hufLlg5kA_Tfz7RKdCPBvvnZ3kbPaQ';
-const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${photosToLoad}`;
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${photosToLoad}&urls=${imageSize}`;
 
+
+/*<<<< IMAGE LOADED >>>>*/
+function imageLoaded(){
+ imagesLoaded++;
+ if (imagesLoaded === (totalImages)){
+    ready = true;
+    pageLoader.hidden = true;
+    console.log('ready =' + ready)
+ }
+}
 
 /*<<<< HELPER FUNCTION >>>>*/
 
@@ -19,6 +36,16 @@ function setAttributes(element, attributes){
 
 /*<<<< Create elements for links and photos >>>>*/
 function displayFotos(){
+  // ⇓ Reseting imagesLoaded is the answer of the tutorial  
+  imagesLoaded = 0;
+  // ⇓ following solution works, but is not the right answer for the project; 
+  // totalImages = photosArray.length + imagesLoaded;
+  // ⇓ following code woks but won't load more images if is not corrected 
+  totalImages = photosArray.length;
+
+
+  console.log('total images = ' + totalImages);
+
   photosArray.forEach((photo) => {
     // Create <a> to link to unsplash
 
@@ -42,12 +69,15 @@ function displayFotos(){
     // img.setAttribute('alt', photo.alt_description);
     // img.setAttribute('title', photo.alt_description);
 
+    /*<<<< CHECK WHEN EACH IS FINISHE LOADING >>>>*/
+    img.addEventListener('load', imageLoaded);
     // image inside <a>
     item.appendChild(img);
     imageContainer.appendChild(item);
 
   });
 }
+
 
 /*<<<< GET PHOTOS FROM UNSPLASH >>>>*/
 
@@ -61,7 +91,20 @@ async function getPhotosUnsplah(){
     //Catch error
   }
 }
+// Check if scroll is near the page
 
+window.addEventListener('scroll', ()=>{
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready){
+    console.log('ready after scroll = '+ ready);
+    getPhotosUnsplah();
+    ready = false;
+    // console.log(`window.innerHeight: ${window.innerHeight}`);
+  //   console.log(``);
+  //   console.log(``);
+  //   console.log(``);
+  //   console.log('load more');
+  }
+});
 
 //Load
 getPhotosUnsplah();
