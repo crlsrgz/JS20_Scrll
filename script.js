b43c5b97e45d1db1c2da1d154f93849fee3647fc
crlsrgz <c@c.com>
2022-09-05 13:30:23 +0200

    [clean up] Refactor and commented code

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-
-
 const imageContainer = document.getElementById('image-container');
-const pageLoader = document.getElementById('page-loader');
+const pagePreloader = document.getElementById('page-loader');
 
 let ready = false;
 let imagesLoaded = 0;
@@ -11,7 +9,7 @@ let photosArray = [];
 
 // unplash API
 const photosToLoad = 3;
-const imageSize = "regular";
+const imageSize = "small";
 const apiKey = '7B31_IL-q-490hufLlg5kA_Tfz7RKdCPBvvnZ3kbPaQ';
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${photosToLoad}&urls=${imageSize}`;
 
@@ -21,14 +19,14 @@ function imageLoaded(){
  imagesLoaded++;
  if (imagesLoaded === (totalImages)){
     ready = true;
-    pageLoader.hidden = true;
-    console.log('ready =' + ready)
+    pagePreloader.hidden = true;
+    // console.log('ready =' + ready)
  }
 }
 
 /*<<<< HELPER FUNCTION >>>>*/
 
-function setAttributes(element, attributes){
+function setTagAttributes(element, attributes){
   for(const key in attributes){
     element.setAttribute(key, attributes[key])
   }
@@ -38,36 +36,39 @@ function setAttributes(element, attributes){
 function displayFotos(){
   // ⇓ Reseting imagesLoaded is the answer of the tutorial  
   imagesLoaded = 0;
-  // ⇓ following solution works, but is not the right answer for the project; 
+  // ⇓ following solution works, but is not the right answer for the project
   // totalImages = photosArray.length + imagesLoaded;
-  // ⇓ following code woks but won't load more images if is not corrected 
+  // ⇓ following code woks but won't load more images if imagesLoaded is not set to 0
   totalImages = photosArray.length;
 
-
-  console.log('total images = ' + totalImages);
+  // console.log('total images = ' + totalImages);
 
   photosArray.forEach((photo) => {
     // Create <a> to link to unsplash
-
     const item = document.createElement('a');
-    setAttributes(item,{
-      href:photo.links.html,
-      target: '_blank'
-    })
+
     // item.setAttribute('href', photo.links.html);
     // item.setAttribute('target','_blank');
+    // ⇓ Replaced: item.setAttributes with the setTagAttributes.
+
+    setTagAttributes(item,{
+      href:photo.links.html,
+      target: '_blank'
+    })    
 
     //create <img>
-
     const img = document.createElement('img');
-    setAttributes(img, {
+
+    // img.setAttribute('src', photo.urls.regular);
+    // img.setAttribute('alt', photo.alt_description);
+    // img.setAttribute('title', photo.alt_description);
+    // ⇓ Replaced: item.setAttributes with the setTagAttributes.
+
+    setTagAttributes(img, {
       src: photo.urls.regular,
       alt: photo.alt_description,
       title: photo.alt_description,
     })
-    // img.setAttribute('src', photo.urls.regular);
-    // img.setAttribute('alt', photo.alt_description);
-    // img.setAttribute('title', photo.alt_description);
 
     /*<<<< CHECK WHEN EACH IS FINISHE LOADING >>>>*/
     img.addEventListener('load', imageLoaded);
@@ -86,23 +87,26 @@ async function getPhotosUnsplah(){
     const response = await fetch(apiUrl);
     photosArray = await response.json();
     displayFotos();
-
+    
   } catch (error){
     //Catch error
   }
 }
-// Check if scroll is near the page
+
+/*<<<< Check if scroll is near the page >>>>*/
 
 window.addEventListener('scroll', ()=>{
   if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready){
     console.log('ready after scroll = '+ ready);
     getPhotosUnsplah();
     ready = false;
-    // console.log(`window.innerHeight: ${window.innerHeight}`);
-  //   console.log(``);
-  //   console.log(``);
-  //   console.log(``);
-  //   console.log('load more');
+    
+    // ⇓ Check heights of the document and window
+    console.log(`window.innerHeight: ${window.innerHeight}`);
+    console.log(`window.scrollY: ${window.scrollY}` );
+    console.log(`window.innerHeight + window.scrollY: ${window.innerHeight + window.scrollY}` );
+    console.log(`document.body.offsetHeight - 1000: ${document.body.offsetHeight - 1000}`);    
+    console.log('load more');
   }
 });
 
